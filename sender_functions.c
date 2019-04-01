6dc27b42a51cab47dd2e92cba1e4a56f434cfb29
George <giwrgosliak_1998@hotmail.com>
2019-04-01 11:36:15 +0300

    communication protocol

---
+++ sender_functions.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <dirent.h>
+#include <fcntl.h>
+
+
